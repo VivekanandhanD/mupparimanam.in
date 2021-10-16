4e6f05a8595bb8f39367f61c55839c2b77ced637
Vivekanandhan D <dvivekanandhan@hotmail.com>
2021-10-16 22:43:35 +0530

    correction

--- web/urls.py
+++ web/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('signup', views.signup, name='signup'),
-    path('login', views.signin, name='login')
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.signin, name='login')
 ]
