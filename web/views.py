ce7c41f1dff12333549ddc2db7671d48e0763f6c
Vivekanandhan D <dvivekanandhan@hotmail.com>
2021-10-16 22:15:43 +0530

    login added

--- web/views.py
+++ web/views.py
@@ -1,5 +1,38 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import render, redirect
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('/')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return  redirect('/')
+        else:
+            form = AuthenticationForm()
+            return render(request, 'login.html', {'form': form})
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
