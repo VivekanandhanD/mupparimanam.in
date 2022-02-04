558efb840f72d831282bc0bf9c5c82d1884081b8
Vivekanandhan D <dvivekanandhan@hotmail.com>
2022-02-04 22:31:12 +0530

    rdfhdfh

--- mupparimanam/settings.py
+++ mupparimanam/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mupparimanam',
         'USER': 'Vivek',
-        'PASSWORD': '',
+        'PASSWORD': 'Keviv07!@',
         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
     }
